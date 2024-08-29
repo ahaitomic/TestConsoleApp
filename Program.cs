798159b737a7045bc7de9b473a434cc7df298bfd
Adrian Haitomic <Adrian.Haitomic@gmail.com>
2024-08-29 15:22:33 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			var numberFilter = new NumberFilter();
+			var numbers = new List<int> { 5, 3, 8, 6, 2, 9, 4, 7, 1, 10 };
+
+			var sortedEvenNumbers = numberFilter.FilterAndSortEvenNumbers(numbers);
+
+			Console.WriteLine("Sorted Even Numbers: " + string.Join(", ", sortedEvenNumbers));
 		}
 	}
 }
